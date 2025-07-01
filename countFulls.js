c8e3961b0917da60712cd43c1f163f8612f5ee70
JoaoDias28 <82152237+JoaoDias28@users.noreply.github.com>
2025-07-01 11:37:33 +0100

    Update countFulls.js

--- countFulls.js
+++ countFulls.js
@@ -240,9 +240,9 @@ function calculateEverything() {
                         villageData[thisID] = [];
                         $.each(game_data.units, function (index) {
                             unitName = game_data.units[index];
-                            if (rows.eq(rowNr).children().not(':first').eq(index).text().trim() != '?') {
-                                villageData[thisID][unitName] = rows.eq(rowNr).children().not(':first').eq(index).text().trim();
-                                villageData["total"][unitName] += parseInt(rows.eq(rowNr).children().not(':first').eq(index).text().trim());
+                            if (rows.eq(rowNr).children().not(':first').eq(index + 1).text().trim() != '?') {
+                                villageData[thisID][unitName] = rows.eq(rowNr).children().not(':first').eq(index + 1).text().trim();
+                                villageData["total"][unitName] += parseInt(rows.eq(rowNr).children().not(':first').eq(index + 1).text().trim());
                             }
                             else {
                                 villageData[thisID][unitName] = 0;
